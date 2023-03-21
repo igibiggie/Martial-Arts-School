ad228729dc229768ad24bc41108d6d6552861462
Igor Leleń <i.lelen@be-tse.com>
2023-03-21 18:09:26 +0100

    Stworzenie kontrolerów i podstawowych widoków listy dla klasy Lesson oraz Member

---
+++ MartialArtsSchool/Controllers/LessonController.cs
@@ -0,0 +1,21 @@
+﻿using MartialArtsSchool.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MartialArtsSchool.Controllers
+{
+    public class LessonController : Controller
+    {
+        private readonly MarArtDbContext _db;
+
+        public LessonController(MarArtDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Lesson> lessons = _db.Lessons;
+            return View(lessons);
+        }
+    }
+}
