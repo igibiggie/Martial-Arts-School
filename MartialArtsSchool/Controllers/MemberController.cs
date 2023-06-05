9f68fbb38fb08757413ea81b701930b24d72782e
Igor.Lelen <igibiggie@gmail.com>
2023-06-05 17:06:55 +0200

    Added CRUD detail pages on Member + methods in Member controller

--- MartialArtsSchool/Controllers/MemberController.cs
+++ MartialArtsSchool/Controllers/MemberController.cs
@@ -17,5 +17,111 @@ namespace MartialArtsSchool.Controllers
             IEnumerable<Member> members = _db.Members;
             return View(members);
         }
+
+        //GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST CREATE
+        [HttpPost]
+        public IActionResult Create(Member obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Members.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Member created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET VIEW
+        public IActionResult View(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Members.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(categoryFromDb);
+        }
+        // bez metody post, bo nic nie edytuję
+
+        //GET EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Members.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(categoryFromDb);
+        }
+
+        //POST EDIT
+        [HttpPost]
+        public IActionResult Edit(Member obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Members.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Members updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Members.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST DELETE
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Members.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Members.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Member deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
