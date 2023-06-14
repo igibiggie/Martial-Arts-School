0388d0cc2bef95e7796b38d91e6a6cf8e04ba603
Igor.Lelen <igibiggie@gmail.com>
2023-06-14 23:23:42 +0200

    added model validation and css styles

--- MartialArtsSchool/Models/Lesson.cs
+++ MartialArtsSchool/Models/Lesson.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MartialArtsSchool.Models;
 
@@ -7,8 +8,13 @@ public partial class Lesson
 {
     public int IdLesson { get; set; }
 
+    [Required, MinLength(2, ErrorMessage = "Należy wpisać minimum 2 znaki"),
+     MaxLength(8, ErrorMessage = "Wpisano zbyt wiele znaków"),
+     RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Proszę użyć tylko liter")]
     public string Name { get; set; } = null!;
 
+    [MinLength(20, ErrorMessage = "Należy wpisać minimum 20 znaków"),
+     MaxLength(255, ErrorMessage = "Wpisano zbyt wiele znaków")]
     public string Description { get; set; } = null!;
 
     public virtual ICollection<Member> IdMemebers { get; } = new List<Member>();
